17df55e67592e70ea726a3507874cbc18f56118e
sadiyabhbb <sadiyabhbb@proton.me>
2025-07-06 00:20:56 +0600

    Update admin.js

--- commands/admin.js
+++ commands/admin.js
@@ -1,8 +1,10 @@
 const { ADMIN_UID, ADMIN_USERNAME } = require('../config/botConfig');
-const { saveDB, userDB } = require('../utils/db');
+const { loadDB, saveDB } = require('../utils/db');
 
 module.exports = (bot) => {
-  // /approve command
+  const userDB = loadDB();
+
+  // /approve UID
   bot.onText(/\/approve (\d+)/, (msg, match) => {
     if (msg.from.username !== ADMIN_USERNAME && msg.from.id !== ADMIN_UID) return;
 
@@ -10,7 +12,8 @@ module.exports = (bot) => {
     if (!userDB.approved.includes(uid)) {
       userDB.approved.push(uid);
       userDB.pending = userDB.pending.filter(id => id !== uid);
-      saveDB();
+      saveDB(userDB);
+
       bot.sendMessage(uid, '✅ Your access has been approved by admin!');
       bot.sendMessage(msg.chat.id, `✅ Approved UID: \`${uid}\``, { parse_mode: 'Markdown' });
     } else {
@@ -18,7 +21,7 @@ module.exports = (bot) => {
     }
   });
 
-  // /ban command
+  // /ban UID
   bot.onText(/\/ban (\d+)/, (msg, match) => {
     if (msg.from.username !== ADMIN_USERNAME && msg.from.id !== ADMIN_UID) return;
 
@@ -26,19 +29,21 @@ module.exports = (bot) => {
     userDB.banned.push(uid);
     userDB.approved = userDB.approved.filter(id => id !== uid);
     userDB.pending = userDB.pending.filter(id => id !== uid);
-    saveDB();
+    saveDB(userDB);
+
     bot.sendMessage(uid, '🚫 You have been banned by admin.');
     bot.sendMessage(msg.chat.id, `🚫 Banned UID: \`${uid}\``, { parse_mode: 'Markdown' });
   });
 
-  // /remove command
+  // /remove UID
   bot.onText(/\/remove (\d+)/, (msg, match) => {
     if (msg.from.username !== ADMIN_USERNAME && msg.from.id !== ADMIN_UID) return;
 
     const uid = parseInt(match[1]);
     userDB.pending = userDB.pending.filter(id => id !== uid);
     userDB.approved = userDB.approved.filter(id => id !== uid);
-    saveDB();
+    saveDB(userDB);
+
     bot.sendMessage(msg.chat.id, `🗑️ Removed UID: \`${uid}\``, { parse_mode: 'Markdown' });
   });
 };
