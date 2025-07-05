8f365d3e8f90e120049989fdd70f9f2567f03920
sadiyabhbb <sadiyabhbb@proton.me>
2025-07-06 00:00:57 +0600

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -1,1 +1,13 @@
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
 
+const bot = new TelegramBot(process.env.BOT_TOKEN, {
+  polling: true,
+  fileDownloadOptions: {
+    headers: {
+      'User-Agent': 'Telegram Bot'
+    }
+  }
+});
+
+module.exports = bot;
