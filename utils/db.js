111e399971c3fcc6f7ef64e210a8ce9e769cbb4f
sadiyabhbb <sadiyabhbb@proton.me>
2025-07-05 23:49:37 +0600

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 
+const DB_PATH = path.join(__dirname, '..', 'users.json');
+let userDB = { approved: [], pending: [], banned: [] };
+
+if (fs.existsSync(DB_PATH)) {
+  userDB = JSON.parse(fs.readFileSync(DB_PATH));
+}
+
+function saveDB() {
+  fs.writeFileSync(DB_PATH, JSON.stringify(userDB, null, 2));
+}
+
+module.exports = { userDB, saveDB };
