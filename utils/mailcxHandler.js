776d499a0e5a435e76a36ee9ef20fe37953a324f
sadiyabhbb <sadiyabhbb@proton.me>
2025-07-07 14:51:21 +0600

    Update mailcxHandler.js

--- utils/mailcxHandler.js
+++ utils/mailcxHandler.js
@@ -1,17 +1,21 @@
 const axios = require('axios');
 const crypto = require('crypto');
 
+// ডোমেইন তালিকা
 const domains = ['qabq.com', 'nqmo.com', 'end.tw', 'uuf.me', '6n9.net'];
 
+// র্যান্ডম ইমেইল তৈরি করার ফাংশন
 function generateRandomEmail() {
   const random = crypto.randomBytes(5).toString('hex');
   const domain = domains[Math.floor(Math.random() * domains.length)];
   return `${random}@${domain}`;
 }
 
+// ইনবক্স ফেচ করার ফাংশন
 async function fetchInbox(email) {
   const [name, domain] = email.split('@');
   const url = `https://api.mail.cx/mailbox/${name}?domain=${domain}`;
+  
   try {
     const { data } = await axios.get(url);
     return data || [];
@@ -21,9 +25,11 @@ async function fetchInbox(email) {
   }
 }
 
+// নির্দিষ্ট ইমেইল ফেচ করার ফাংশন
 async function fetchFullEmail(email, id) {
   const [name, domain] = email.split('@');
   const url = `https://api.mail.cx/mailbox/${name}/${id}?domain=${domain}`;
+  
   try {
     const { data } = await axios.get(url);
     return data;
@@ -33,6 +39,7 @@ async function fetchFullEmail(email, id) {
   }
 }
 
+// মডিউল এক্সপোর্ট
 module.exports = {
   generateRandomEmail,
   fetchInbox,
