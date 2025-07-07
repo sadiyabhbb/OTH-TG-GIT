fab81409fae0b4ca5e0cdba092c4e0255550d645
sadiyabhbb <sadiyabhbb@proton.me>
2025-07-07 12:54:53 +0600

    Update mailcxHandler.js

--- utils/mailcxHandler.js
+++ utils/mailcxHandler.js
@@ -1,18 +1,14 @@
-// 📁 utils/mailcxHandler.js
 const axios = require('axios');
 const crypto = require('crypto');
 
-// 🔐 Random Mail.cx ইমেইল জেনারেটর
 function generateRandomEmail() {
   const random = crypto.randomBytes(5).toString('hex');
   return `${random}@mail.cx`;
 }
 
-// 📥 নির্দিষ্ট ইমেইলের ইনবক্স লিস্ট ফেচ করে
 async function fetchInbox(email) {
   const name = email.split('@')[0];
   const url = `https://api.mail.cx/mailbox/${name}`;
-
   try {
     const { data } = await axios.get(url);
     return data || [];
@@ -22,11 +18,9 @@ async function fetchInbox(email) {
   }
 }
 
-// 📧 নির্দিষ্ট ইমেইল ID দিয়ে সম্পূর্ণ মেইল ফেচ করে
 async function fetchFullEmail(email, id) {
   const name = email.split('@')[0];
   const url = `https://api.mail.cx/mailbox/${name}/${id}`;
-
   try {
     const { data } = await axios.get(url);
     return data;
