0ee7124c4150342588fb82eb9006057b482d5412
sadiyabhbb <sadiyabhbb@proton.me>
2025-07-07 14:03:13 +0600

    Update mailcxHandler.js

--- utils/mailcxHandler.js
+++ utils/mailcxHandler.js
@@ -1,23 +1,17 @@
 const axios = require('axios');
 const crypto = require('crypto');
 
-const DOMAINS = [
-  'qabq.com',
-  'nqmo.com',
-  'end.tw',
-  'uuf.me',
-  '6n9.net'
-];
+const domains = ['qabq.com', 'nqmo.com', 'end.tw', 'uuf.me', '6n9.net'];
 
 function generateRandomEmail() {
   const random = crypto.randomBytes(5).toString('hex');
-  const domain = DOMAINS[Math.floor(Math.random() * DOMAINS.length)];
+  const domain = domains[Math.floor(Math.random() * domains.length)];
   return `${random}@${domain}`;
 }
 
 async function fetchInbox(email) {
-  const name = email.split('@')[0];
-  const url = `https://api.mail.cx/mailbox/${name}`;
+  const [name, domain] = email.split('@');
+  const url = `https://api.mail.cx/mailbox/${name}?domain=${domain}`;
   try {
     const { data } = await axios.get(url);
     return data || [];
@@ -28,8 +22,8 @@ async function fetchInbox(email) {
 }
 
 async function fetchFullEmail(email, id) {
-  const name = email.split('@')[0];
-  const url = `https://api.mail.cx/mailbox/${name}/${id}`;
+  const [name, domain] = email.split('@');
+  const url = `https://api.mail.cx/mailbox/${name}/${id}?domain=${domain}`;
   try {
     const { data } = await axios.get(url);
     return data;
