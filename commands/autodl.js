02dc8ad4a612ffe1ec8901a9f28d7601c3fcdcbc
likhonbotcheckok <kkhjkoooghni@hotmail999.com>
2025-07-11 10:15:18 +0600

    Update autodl.js

--- commands/autodl.js
+++ commands/autodl.js
@@ -45,7 +45,7 @@ module.exports = (bot) => {
         fs.writeFileSync(filePath, Buffer.from(file.data, "binary"));
 
         // লিংক বাদ দিয়ে শুধু ক্যাপশন সহ ফাইল পাঠানো
-        await bot.sendvideo(chatId, filePath, { caption });
+        await bot.sendVideo(chatId, filePath, { caption });
 
         fs.unlinkSync(filePath); // Remove after sending
 
